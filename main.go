40341bac591d66a74a33816552f985187c44810a
ardentseeker <krdivakar65@gmail.com>
2025-11-28 11:54:36 +0530

    this commit holds go routine usage and remember for multiple task to dodge race condition we use mutex

--- main.go
+++ main.go
@@ -4,14 +4,51 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"sync"
 )
 
 func main() {
-	Raw_Order_Converter()
+	// Using WaitGroup to wait for goroutine to finish
+	// Note :- suitable for multiple goroutines
+	// if you will call it multiple times race condition may occur on orders slice
+	// so proper synchronization is needed in that case and for that mutex can be used
+
+	/*
+		//here the variable is defined at top level and the method is tightly coupled with it.
+		var wg sync.WaitGroup
+		wg.Add(3)
+		go func() {
+			defer wg.Done()
+			log.Println("Starting Raw Order Conversion in Goroutine")
+			Raw_Order_Converter()
+		}()
+		wg.Wait()
+	*/
+
+	/* Using channel to signal completion of goroutine
+	 Note :- only suitable for single goroutine
+	done := make(chan bool)
+	go func() {
+		Raw_Order_Converter()
+		done <- true
+	}()
+	log.Println("Waiting for Goroutine to finish")
+	// this can also be called directly without goroutine
+
+	//Raw_Order_Converter()
+
+	*/
+
+	//to avoid global variable coupling we can pass the waitgroup as parameter to the function
+	var wg sync.WaitGroup
+	wg.Add(1)
+	Raw_Order_Converter(&wg)
+	wg.Wait()
+	log.Println("Goroutine finished execution")
 	fmt.Println(orders)
 }
 
-func Raw_Order_Converter() {
+func Raw_Order_Converter(wg *sync.WaitGroup) {
 
 	for _, rawOrder := range raw_Orders {
 		var newOrder order
@@ -22,6 +59,8 @@ func Raw_Order_Converter() {
 		}
 		orders = append(orders, newOrder)
 	}
+	wg.Done()
+
 }
 
 var raw_Orders = []string{
